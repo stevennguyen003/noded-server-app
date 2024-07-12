9df1872fcdff304f6aff955f4edcab56a029d7d7
stevennguyen003 <96093087+stevennguyen003@users.noreply.github.com>
2024-07-12 19:09:27 -0400

    added unique invite links to groups

--- Groups/schema.js
+++ Groups/schema.js
@@ -1,4 +1,10 @@
 import mongoose from "mongoose";
+import crypto from 'crypto';
+
+// Function to generate a random invite link
+function generateInviteLink() {
+    return crypto.randomBytes(4).toString('hex');
+}
 
 // Represents how the group collection is structured in the database
 const groupSchema = new mongoose.Schema({
@@ -12,6 +18,30 @@ const groupSchema = new mongoose.Schema({
         }
     },
     profilePicture: String,
+    inviteLink: { 
+        type: String, 
+        default: generateInviteLink,
+        unique: true,
+        required: true
+    }
 },
-    { collection: "groups" });
+{ collection: "groups" });
+
+// Middleware to ensure uniqueness of invite link
+groupSchema.pre('save', async function(next) {
+    if (this.isNew) {
+        const Group = mongoose.model('Group', groupSchema);
+        let isUnique = false;
+        while (!isUnique) {
+            const existingGroup = await Group.findOne({ inviteLink: this.inviteLink });
+            if (!existingGroup) {
+                isUnique = true;
+            } else {
+                this.inviteLink = generateInviteLink();
+            }
+        }
+    }
+    next();
+});
+
 export default groupSchema;
